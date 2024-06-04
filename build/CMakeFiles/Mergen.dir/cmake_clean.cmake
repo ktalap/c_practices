c4eea9238b4e9873b7ed30cdccbf7f900e30c56b
Dexter <dexter@obeluss.com>
2024-06-04 17:07:44 +0500

    Added main.c
    Change the structure a bit with the ./src folder. Will likely write an array of dynamic size after this.

---
+++ build/CMakeFiles/Mergen.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Mergen.dir/src/main.c.o"
+  "CMakeFiles/Mergen.dir/src/main.c.o.d"
+  "Mergen"
+  "Mergen.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Mergen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Mergen.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Mergen.
+# This may be replaced when dependencies are built.
